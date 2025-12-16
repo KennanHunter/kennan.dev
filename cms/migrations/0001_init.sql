faf766bb1cd4ba1f9f6b4c4cb45cdc362aca26dc
Kennan <kennanhunter5@gmail.com>
2025-12-16 01:43:53 -0500

    init cms

---
+++ cms/migrations/0001_init.sql
@@ -0,0 +1,39 @@
+-- Migration number: 0001 	 2025-12-16T06:04:02.922Z
+
+CREATE TABLE photos (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE photo_versions (
+    id SERIAL PRIMARY KEY,
+    photo_id INTEGER REFERENCES photos (id) ON DELETE CASCADE,
+    version_name TEXT NOT NULL,
+    url TEXT NOT NULL,
+    width INTEGER NOT NULL,
+    height INTEGER NOT NULL
+);
+
+CREATE TABLE photo_version_blur (
+    photo_version_id INTEGER PRIMARY KEY,
+    blur_data TEXT NOT NULL,
+    FOREIGN KEY (photo_version_id) REFERENCES photo_versions (id) ON DELETE CASCADE
+)
+
+CREATE INDEX idx_photo_versions_photo_id ON photo_versions (photo_id);
+
+CREATE TABLE photo_collection (
+    name TEXT PRIMARY KEY,
+    description TEXT,
+    created_at INTEGER NOT NULL,
+    created_by TEXT
+)
+
+CREATE TABLE photo_collection_photos (
+    photo_collection_name TEXT REFERENCES photo_collection (name) ON DELETE CASCADE,
+    photo_id INTEGER REFERENCES photos (id) ON DELETE CASCADE,
+    PRIMARY KEY (
+        photo_collection_name,
+        photo_id
+    )
+);
---
+++ cms/migrations/0002_users.sql
@@ -0,0 +1,23 @@
+CREATE TABLE user(
+    discord_client_id TEXT PRIMARY KEY,
+    username TEXT NOT NULL,
+    last_login INTEGER,
+    invited_at INTEGER,
+    invited_by TEXT
+)
+
+CREATE TABLE permission_group (
+    name TEXT PRIMARY KEY,
+    description TEXT,
+    created_at INTEGER,
+    created_by TEXT
+)
+
+CREATE TABLE user_permission_group (
+    user_discord_client_id TEXT REFERENCES user(discord_client_id) ON DELETE CASCADE,
+    permission_group_name TEXT REFERENCES permission_group (name) ON DELETE CASCADE,
+    PRIMARY KEY (
+        user_discord_client_id,
+        permission_group_name
+    )
+);
